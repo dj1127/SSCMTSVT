da3ca19a5f1684bb4a43935f0e8628cae354c2f9
Dogyu Jun <dokyu1127@gmail.com>
2023-03-10 12:59:51 -0500

    change type name

--- read_input.f90
+++ read_input.f90
@@ -1,4 +1,4 @@
-subroutine read_input(TRV80)
+subroutine read_input(const)
 
 
     use defineAircraftProperties
@@ -8,7 +8,7 @@ subroutine read_input(TRV80)
     
 
     
-    type(trv80_2rot) :: TRV80
+    type(const_struct) :: const
 
     character*80 :: EchoLine
     integer :: i
@@ -24,87 +24,87 @@ subroutine read_input(TRV80)
 !    end do
 
     ! aircraft weight
-    read(10, 9040) TRV80%W
+    read(10, 9040) const%W
     
-    !write(*,*) TRV80%W
+    !write(*,*) const%W
     
     ! number of rotors [ND]
-    read(10, 9040) TRV80%nrot
+    read(10, 9040) const%nrot
     
-    !write(*,*) TRV80%nRot
+    !write(*,*) const%nRot
 
     ! number of blades for each rotor [ND]
-    read(10, 9040) TRV80%Nb(1)
-    read(10, 9040) TRV80%Nb(2)
+    read(10, 9040) const%Nb(1)
+    read(10, 9040) const%Nb(2)
     
-    !write(*,*) TRV80%Nb
+    !write(*,*) const%Nb
 
     ! rotor radius [ft] (for now assum equal radius across all rotors)
-    read(10, 9040) TRV80%R(1)
-    read(10, 9040) TRV80%R(2)
+    read(10, 9040) const%R(1)
+    read(10, 9040) const%R(2)
     
-    !write(*,*) TRV80%R
+    !write(*,*) const%R
     
     ! blade chord
-    read(10, 9040) TRV80%c(1)
-    read(10, 9040) TRV80%c(2)
+    read(10, 9040) const%c(1)
+    read(10, 9040) const%c(2)
     
-    !write(*,*) TRV80%c
+    !write(*,*) const%c
     
     ! rotor angular speed [rad/s]
-    read(10, 9040) TRV80%omega(1)
-    read(10, 9040) TRV80%omega(2)
+    read(10, 9040) const%omega(1)
+    read(10, 9040) const%omega(2)
     
-    !write(*,*) TRV80%omega
+    !write(*,*) const%omega
     
     ! rotor verse of rotation [ND]: 1: CCW, -1: CCW
-    read(10, 9040) TRV80%sRot(1)
-    read(10, 9040) TRV80%sRot(2)
+    read(10, 9040) const%sRot(1)
+    read(10, 9040) const%sRot(2)
     
-    !write(*,*) TRV80%sRot
+    !write(*,*) const%sRot
     
     ! twist angle for each rotor [rad]
-    read(10, 9040) TRV80%twist(1)
-    read(10, 9040) TRV80%twist(2)
+    read(10, 9040) const%twist(1)
+    read(10, 9040) const%twist(2)
     
-    TRV80%twist(1) = TRV80%twist(1)*(pi/180)
-    TRV80%twist(2) = TRV80%twist(2)*(pi/180)
+    const%twist(1) = const%twist(1)*(pi/180)
+    const%twist(2) = const%twist(2)*(pi/180)
     
     
     
     ! rotor solidity [ND]
     do i = 1,2
-        TRV80%sigma(i) = (TRV80%Nb(i)*TRV80%c(i)*pi)/TRV80%R(i)
+        const%sigma(i) = (const%Nb(i)*const%c(i)*pi)/const%R(i)
     end do
     
     
     ! rotor blade lift curve slope [1/rad]
-    read(10, 9040) TRV80%a0(1)
-    read(10, 9040) TRV80%a0(2)
+    read(10, 9040) const%a0(1)
+    read(10, 9040) const%a0(2)
     
     
     ! blade drag coefficients [ND]
-    read(10, 9040) TRV80%delta0(1)
-    read(10, 9040) TRV80%delta0(2)
+    read(10, 9040) const%delta0(1)
+    read(10, 9040) const%delta0(2)
     
     
-    read(10, 9040) TRV80%delta2(1)
-    read(10, 9040) TRV80%delta2(2)
+    read(10, 9040) const%delta2(1)
+    read(10, 9040) const%delta2(2)
     
     
     ! rotors position in body axes (i.e., with respect to CG) [ft]
     ! (if changing any of these, re-run G matrix computation)
-    read(10, 9040) TRV80%xRot(1)
-    read(10, 9040) TRV80%xRot(2)
-    !write(*,*) TRV80%xRot
+    read(10, 9040) const%xRot(1)
+    read(10, 9040) const%xRot(2)
+    !write(*,*) const%xRot
     
 
-    read(10, 9040) TRV80%yRot(1)
-    read(10, 9040) TRV80%yRot(2)
+    read(10, 9040) const%yRot(1)
+    read(10, 9040) const%yRot(2)
     
     
-    read(10, 9040) TRV80%zRot(1)
-    read(10, 9040) TRV80%zRot(2)
+    read(10, 9040) const%zRot(1)
+    read(10, 9040) const%zRot(2)
     
     
     
