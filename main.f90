3fd4bdb44e8233df0ee2ede94670c58a7df40079
dj1127 <dokyu1127@gmail.com>
2023-03-12 01:27:02 -0500

    all syntax error fixed

--- main.f90
+++ main.f90
@@ -7,7 +7,7 @@ Program main
     ! University of Maryland
     ! saetti@umd.edu
     !
-    ! DATE
+    ! DATE 
     ! 8/19/2022 
     !
     ! DESCRIPTION
@@ -86,21 +86,19 @@ Program main
     integer :: i,j
     integer :: iChi, iRot, jRot
     integer :: iAero, jAero
-    integer :: x, y, z, row, col
+    ! integer :: x, y, z, row, col 
     real*8 :: xij, yij, zij
-    real, allocatable :: my_array(:)
     real*8 :: innerInt1(30,360), innerInt2(30,360), innerInt3(30,360)
     real*8 :: interInt1(360), interInt2(360), interInt3(360), &
     interInt4(360), interInt5(360),interInt6(360)
     integer :: a,b,c,d,e
-    real*8 :: onemat(1,2) ! need to be edited
     
     
 
     
     !----- integration param -----
     real*8 :: res
-    real*8 :: K(360), bs_K(360)
+    real*8 :: K(360)
     real*8 :: innerInt1_t(1,30), innerInt2_t(1,30), innerInt3_t(1,30)
     real*8 :: innerInt1_1D(30), innerInt2_1D(30), innerInt3_1D(30)
     real*8 :: inner1_calc(30), inner2_calc(30), inner3_calc(30),&
@@ -109,8 +107,12 @@ Program main
     inter4c_calc(360), inter5c_calc(360), inter6c_calc(360)
     
     !------- Trim Parameter -------
+    integer :: NSTATES
     real*8 :: mat_z14(1,14)
     integer :: index_calc
+    real*8 :: itrim
+    real*8 :: NULL(12,1)
+
     
 
     
@@ -274,7 +276,7 @@ Program main
                                 
                                 ! intermediate integral
                                 if (intMethod == 1) then
-                                res = 0.
+
                                 innerInt1_t = transpose(innerInt1(1:30,jAero:jAero))
                                 do i = 1,30
                                     innerInt1_1d(i) = innerInt1_t(i,1)
@@ -304,22 +306,19 @@ Program main
                                 end do
                                 call trapz(const%rSeg,inner3_calc,30,res)
                                 interInt3(jAero) = res
-                                
-                                res = 0.
+                            
                                 do i = 1,30
                                     inner4_calc(i) = innerInt1_1D(i)*(const%rSeg(i)**2)
                                 end do
                                 call trapz(const%rSeg,inner4_calc,30,res)
-                                interInt4(jAero) = res
-                                
-                                res = 0.
+                                interInt4(jAero) = res                               
+
                                 do i = 1,30
                                     inner5_calc(i) = innerInt2_1D(i)*(const%rSeg(i)**2)
                                 end do
                                 call trapz(const%rSeg,inner5_calc,30,res)
                                 interInt5(jAero) = res
                                 
-                                res = 0.
                                 do i = 1,30
                                     inner6_calc(i) = innerInt3_1D(i)*(const%rSeg(i)**2)
                                 end do
@@ -332,40 +331,40 @@ Program main
                         
                             ! outer integral
                             if (intMethod == 1) then
-                            res = 0.
+
                             call trapz(const%psiv,interInt1,360,res)
                             G(1,1,iRot,jRot,iChi) = -(1/(4*(pi**2)))*res
                             
-                            res = 0.
+
                             call trapz(const%psiv,interInt2,360,res)
                             G(1,2,iRot,jRot,iChi) = -(1/(4*(pi**2)))*res
                             
-                            res = 0.
+
                             call trapz(const%psiv,interInt3,360,res)
                             G(1,3,iRot,jRot,iChi) = -(1/(4*(pi**2)))*res
                             
-                            res = 0.
+
                             do i = 1,360
                                 inter4c_calc(i) = interInt4(i)*dcos(const%psiv(i))
                             end do
                             call trapz(const%psiv,inter4c_calc,360,res)
                             G(2,1,iRot,jRot,iChi) = -(1/(pi**2))*res
                             
-                            res = 0.
+
                             do i = 1,360
                                 inter5c_calc(i) = interInt5(i)*dcos(const%psiv(i))
                             end do
                             call trapz(const%psiv,inter5c_calc,360,res)
                             G(2,2,iRot,jRot,iChi) = -(1/(pi**2))*res
                             
-                            res = 0.
+
                             do i = 1,360
                                 inter6c_calc(i) = interInt6(i)*dcos(const%psiv(i))
                             end do
                             call trapz(const%psiv,inter6c_calc,360,res)
                             G(2,3,iRot,jRot,iChi) = -(1/(pi**2))*res
                             
-                            res = 0.
+
                             do i = 1,360
                                 inter4s_calc(i) = interInt4(i)*sin(const%psiv(i))
                             end do
@@ -414,250 +413,268 @@ Program main
     ! set G matrix to zeros
         G(1:3,1:3,1:const%nRot,1:const%nRot,1:size(const%chiv)) = 0
     end if
-!
-!
-!    
-!    ! relative scaling of states (for each rotor)
-!    do i = 1,6
-!        XSCALE(i,1) = 1
-!    end do
-!    ! assemble relative scale of states for all rotors - HARDCODED
-!    const%XSCALE(1:6,1) = XSCALE(1:6,1)
-!    const%XSCALE(7:12,1) = XSCALE(1:6,1)
-!    
-!    ! state perturbation size for trim and linearization (for each rotor)
-!    do j = 1,6
-!        DELXLIN(j) = (pi/180.)*0.1
-!    end do
-!    
-!    ! assemble relative scale of states for all rotors 
-!    const%DELXLIN(1:6) = DELXLIN(1:6)
-!    const%DELXLIN(7:12) = DELXLIN(1:6)
-!    
-!    ! control perturbation size for trim and linearization (for each rotor)
-!    DELCLIN(1:3) = 1
-!    DELCLIN(4) = pi/180
-!    DELCLIN(5:7) = 1
-!    DELCLIN(8:10) = pi/180
-!    DELCLIN = DELCLIN*0.1
-!    
-!    ! assemble control perturbation size for all rotors 
-!    const%DELCLIN(1:10) = DELCLIN(1:10)
-!    const%DELCLIN(11:20) = DELCLIN(1:10)
-!    
-!    ! relative scaling of states (for each rotor)
-!    call ones(1,const%nRot,onemat)
-!    do i = 1,2
-!        const%YSCALE(i,1) = onemat(1,i)*10.**(-3)
-!    end do
-!    do i = 3,4
-!        const%YSCALE(i,1) = onemat(1,i-2)*10.**(-4)
-!    end do
-!    do i = 5,6
-!        const%YSCALE(i,1) = onemat(1,i-4)
-!    end do
-!    do i = 7,8
-!        const%YSCALE(i,1) = onemat(1,i-6)
-!    end do
-!    do i = 9,10
-!        const%YSCALE(i,1) = onemat(1,i-8)
-!    end do
-!    
-!    ! number of states 
-!    const%NSTATES = size(conDELXLIN)
-!    ! number of control inputs 
-!    const%NCTRLS = size(conDELCLIN)
-!    ! number of outputs 
-!    const%NOUT = 5*const%nRot
-!    ! trim variables and trim targets 
-!!    if (trimMethod ==1) then
-!!    else if (trimMethod ==2) then
-!!        do i = 1,const%nRot
-!!        
-!!        end do
-!!        
-!!        do i = 1,const%nRot
-!!        
-!!        end do
-!!    else if (trimMethod ==3) then
-!!        do i = 1,const%nRot
-!!        
-!!        end do
-!!        
-!!        do i = 1,const%nRot
-!!        
-!!        end do
-!!    else
-!    
-!!    end if
-!    
-!    
-!
-!!   ---------------------------- TRIM SIMULATION -----------------------------
-!    
-!    ! swashplate inputs for each rotor
-!    swashplate0 = (/ 19.73,0.,0. /)
-!    ! velocities of each rotor
-!    vel0 = (/0,0,0/)
-!    ! angular rates of each rotor
-!    ang0 = (/0,0,0/)
-!    ! initialize input vector
-!    u0 = 0
-!    ! assemble input vector
-!    u0(1:3,1) = swashplate0
-!    u0(4:5,1) = const%omega(1)
-!    u0(6:8,1) = vel0
-!    u0(9:11,1) = ang0
-!    
-!    do i = 1,3
-!        u0(i,1) = swashplate0(i)
-!    end do
-!    do i = 4,5
-!        u0(i,1) = const%omega(i-3)
-!    end do
-!    do i = 6,8
-!        u0(i,1) = vel0(i-5)
-!    end do
-!    do i = 9,11
-!        u0(i,1) = ang0(i-8)
-!    end do
-!    ! initial guess to inflow
-!    lambda0 = (/0.05,0.,0./)
-!    lambdatot0 = (/0.05,0.,0./)
-!    ! initialize state vector
-!    x0 = 0
-!    ! assemble state vector
-!    
-!    ! trim targets
-!    if (trimMethod == 1) then
-!        do i =1,const%NSTATES
-!            targ_des(i,1) = 0
-!        end do
-!    elseif (trimMethod == 2) then
-!        ! desired thrust coefficient
-!        do i = 1,const%nRot
-!            CTdes(i) = (const%W/const%nRot)/(rho*(pi*const%R(i)**2)&
-!            *const%Omega(I)**2*const%R(i)**2)
-!        end do
-!        do i = 1,12
-!            targ_des(i,1) = 0
-!        end do
-!        do i = 13,14
-!            targ_des(i,1) = CTdes(i-12)
-!        end do
-!    elseif (trimMethod == 2) then
-!        ! desired thrust coefficient
-!        do i = 1,const%nRot
-!            Tdes(i) = const%W/const%nRot
-!        end do 
-!        do i = 1,12
-!            targ_des(i,1) = 0
-!        end do
-!        do i = 13,14
-!            targ_des(i,1) = Tdes(i-12)
-!        end do
-!    else
-!        call zero(const%NSTATES,1,mat_z14)
-!        targ_des(1:14,1) = mat_z14(1,1:14)
-!    end if
-!
-!
-!
-!    !trim indueced inflow
-!    print *, 'INDUCED INFLOW'
-!    do i=1,const%nRot
-!        index_calc = 1+(i-1)*3
-!        x0trim_val = x0trim(index_calc)
-!    end do
-!    print 24,'lambda0      = ', x0trim_val
-!    do i=1,const%nRot
-!        index_calc = 2+(i-1)*3
-!        x0trim_val = x0trim(index_calc)
-!    end do
-!    print 24,'lambda1c      = ', x0trim_val
-!    do i=1,const%nRot
-!        index_calc = 3+(i-1)*3
-!        x0trim_val = x0trim(index_calc)
-!    end do
-!    print 24,'lambda1s      = ', x0trim_val
-!    print *,' '
-!    
-!    ! trim total inflow 
-!    print *,'TOTAL INFLOW'
-!    do i=1,const%nRot
-!        index_calc = const%nRot*3+1+(i-1)*3
-!        x0trim_val = x0trim(index_calc)
-!    end do
-!    print 24,'lambda0       = ', x0trim_val
-!    do i=1,const%nRot
-!        index_calc = const%nRot*3+2+(i-1)*3
-!        x0trim_val = x0trim(index_calc)
-!    end do
-!    print 24,'lambda1c      = ', x0trim_val
-!    print *,' '
-!    print *,'lambda1s      = '
-!    do i=1,const%nRot
-!        index_calc = const%nRot*3+3+(i-1)*3
-!        x0trim_val = x0trim(index_calc)
-!    end do
-!    print 24, x0trim_val
-!    print *,' '
-!    
-!    ! trim coefficients of thrust and power, thrust and power 
-!    print *,'PERFORMANCE METRICS'
-!    do i=1,const%nRot
-!        index_calc = 1+(i-1)
-!        y0trim_val = y0trim(index_calc)
-!    end do
-!    print 15,'CT            = ', y0trim_val
-!    do i=1,const%nRot
-!        index_calc = const%nRot*1+1+(i-1)
-!        y0trim_val = y0trim(index_calc)
-!    end do
-!    print 15,'CP            = ', y0trim_val
-!    do i=1,const%nRot
-!        index_calc = const%nRot*2+1+(i-1)
-!        y0trim_val = y0trim(index_calc)
-!    end do
-!    print 52,'T [lb]        = ', y0trim_val
-!    do i=1,const%nRot
-!        index_calc = const%nRot*4+1+(i-1)
-!        y0trim_val = y0trim(index_calc)
-!    end do
-!    print 52,'Q [lb-ft]     = ', y0trim_val
-!    do i=1,const%nRot
-!        index_calc = const%nRot*3+1+(i-1)
-!        y0trim_val = y0trim(index_calc)
-!    end do
-!    print 52,'P [hp]        = ', y0trim_val
-!    print *,' '
-!    
-!    ! trim controls 
-!    print *,'ROTOR CONTROLS'
-!    
-!    do i=1,const%nRot
-!        index_calc = 1+(i-1)*const%NCTRLS/const%nRot
-!        u0trim_val = u0trim(index_calc)
-!    end do
-!    print 21,'theta0 [deg]  = ', u0trim_val
-!    
-!    do i=1,const%nRot
-!        index_calc = 4+(i-1)*const%NCTRLS/const%nRot
-!        u0trim_val = u0trim(index_calc)
-!    end do
-!    print 31,'Omega [rad/s] = ', u0trim_val
-!    print *,' '
-!        
-!
-!24  format(1x, f6.4)
-!
-!15  format(1x, f6.5)
-!
-!52  format(1x, f7.2)
-!
-!21  format(1x, f3.1)
-!
-!31  format(1x, f4.1)
+
+
+    
+    ! relative scaling of states (for each rotor)
+    XSCALE(:,1) = 1
+
+    ! assemble relative scale of states for all rotors - HARDCODED
+    const%XSCALE(1:6,1) = XSCALE(1:6,1)
+    const%XSCALE(7:12,1) = XSCALE(1:6,1)
+    
+    ! state perturbation size for trim and linearization (for each rotor)
+    DELXLIN(:) = (pi/180.)*0.1
+    
+    ! assemble relative scale of states for all rotors 
+    const%DELXLIN(1:6) = DELXLIN(1:6)
+    const%DELXLIN(7:12) = DELXLIN(1:6)
+    
+    ! control perturbation size for trim and linearization (for each rotor)
+    DELCLIN(1:3) = 1
+    DELCLIN(4) = pi/180
+    DELCLIN(5:7) = 1
+    DELCLIN(8:10) = pi/180
+    DELCLIN = DELCLIN*0.1
+    
+    ! assemble control perturbation size for all rotors 
+    const%DELCLIN(1:10) = DELCLIN(1:10)
+    const%DELCLIN(11:20) = DELCLIN(1:10)
+    
+    ! relative scaling of states (for each rotor)
+    do i = 1,2
+        const%YSCALE(i,1) = 1*(10.**(-3))
+    end do
+    do i = 3,4
+        const%YSCALE(i,1) = 1*(10.**(-4))
+    end do
+    do i = 5,10
+        const%YSCALE(i,1) = 1
+    end do
+
+
+    
+    ! number of states 
+    const%NSTATES = size(const%DELXLIN)
+    ! number of control inputs 
+    const%NCTRLS = size(const%DELCLIN)
+    ! number of outputs 
+    const%NOUT = 5*const%nRot
+    ! trim variables and trim targets 
+
+    allocate(const%TRIMVARS(14))
+    allocate(const%TRIMTARG(14))
+
+    NSTATES = const%NSTATES
+
+    if (trimMethod ==1) then
+        ! trim variables
+        const%TRIMVARS = (/ (I, I = 1, NSTATES) /)
+        ! trim targets
+        const%TRIMTARG = (/ (I, I = 1, NSTATES) /)
+    else if (trimMethod ==2) then
+        ! trim variables
+        const%TRIMVARS = (/ (I, I = 1, NSTATES) /)
+        ! add collective pitch to trim variables 
+        do i = 1,const%nRot
+            const%TRIMVARS=(/const%TRIMVARS, const%NSTATES+1+(i-1)*const%NCTRLS/const%nRot/)
+        end do
+        ! trim targets
+        const%TRIMTARG=(/ (I, I = 1, NSTATES) /)
+        do i = 1,const%nRot
+            const%TRIMTARG=(/const%TRIMTARG, const%NSTATES+1+(i-1)/); 
+        end do
+    else if (trimMethod ==3) then
+        ! trim variables
+        const%TRIMVARS = (/ (I, I = 1, NSTATES) /)
+        do i = 1,const%nRot
+            const%TRIMVARS=(/const%TRIMVARS, const%NSTATES+4+(i-1)*const%NCTRLS/const%nRot/)
+        end do
+        ! trim targets
+        const%TRIMTARG = (/ (I, I = 1, NSTATES) /)
+        do i = 1,const%nRot
+            const%TRIMTARG=(/const%TRIMTARG, const%NSTATES+5+(i-1)/);
+        end do
+    else
+        ! trim variables
+        const%TRIMVARS = (/ (I, I = 1, NSTATES) /)
+        ! trim targets
+        const%TRIMTARG = (/ (I, I = 1, NSTATES) /)
+    end if
+   
+  
+  
+
+
+!       ---------------------------- TRIM SIMULATION -----------------------------
+    
+    ! swashplate inputs for each rotor
+    swashplate0 = (/ 19.73,0.,0. /)
+    ! velocities of each rotor
+    vel0 = (/0,0,0/)
+    ! angular rates of each rotor
+    ang0 = (/0,0,0/)
+    ! initialize input vector
+    u0 = 0
+    ! assemble input vector
+    u0(1:3,1) = swashplate0
+    u0(4:5,1) = const%omega(1)
+    u0(6:8,1) = vel0
+    u0(9:11,1) = ang0
+    
+    do i = 1,3
+        u0(i,1) = swashplate0(i)
+    end do
+    do i = 4,5
+        u0(i,1) = const%omega(i-3)
+    end do
+    do i = 6,8
+        u0(i,1) = vel0(i-5)
+    end do
+    do i = 9,11
+        u0(i,1) = ang0(i-8)
+    end do
+    ! initial guess to inflow
+    lambda0 = (/0.05,0.,0./)
+    lambdatot0 = (/0.05,0.,0./)
+    ! initialize state vector
+    x0 = 0
+    ! assemble state vector
+    
+    ! trim targets
+    if (trimMethod == 1) then
+        do i =1,const%NSTATES
+            targ_des(i,1) = 0
+        end do
+    elseif (trimMethod == 2) then
+        ! desired thrust coefficient
+        do i = 1,const%nRot
+            CTdes(i) = (const%W/const%nRot)/(rho*(pi*const%R(i)**2)&
+            *const%Omega(I)**2*const%R(i)**2)
+        end do
+        do i = 1,12
+            targ_des(i,1) = 0
+        end do
+        do i = 13,14
+            targ_des(i,1) = CTdes(i-12)
+        end do
+    elseif (trimMethod == 2) then
+        ! desired thrust coefficient
+        do i = 1,const%nRot
+            Tdes(i) = const%W/const%nRot
+        end do 
+        do i = 1,12
+            targ_des(i,1) = 0
+        end do
+        do i = 13,14
+            targ_des(i,1) = Tdes(i-12)
+        end do
+    else
+        call zero(const%NSTATES,1,mat_z14)
+        targ_des(1:14,1) = mat_z14(1,1:14)
+    end if
+
+    ! trim simulation 
+    call TrimSim(x0,u0,targ_des,const,x0trim,u0trim,itrim); 
+    ! trim output 
+    call CMTSVT(x0trim,u0trim,const,NULL,y0trim);
+
+
+    !trim indueced inflow
+    print *, 'INDUCED INFLOW'
+    do i=1,const%nRot
+        index_calc = 1+(i-1)*3
+        x0trim_val = x0trim(index_calc)
+    end do
+    print 24,'lambda0      = ', x0trim_val
+    do i=1,const%nRot
+        index_calc = 2+(i-1)*3
+        x0trim_val = x0trim(index_calc)
+    end do
+    print 24,'lambda1c      = ', x0trim_val
+    do i=1,const%nRot
+        index_calc = 3+(i-1)*3
+        x0trim_val = x0trim(index_calc)
+    end do
+    print 24,'lambda1s      = ', x0trim_val
+    print *,' '
+    
+    !trim total inflow 
+    print *,'TOTAL INFLOW'
+    do i=1,const%nRot
+        index_calc = const%nRot*3+1+(i-1)*3
+        x0trim_val = x0trim(index_calc)
+    end do
+    print 24,'lambda0       = ', x0trim_val
+    do i=1,const%nRot
+        index_calc = const%nRot*3+2+(i-1)*3
+        x0trim_val = x0trim(index_calc)
+    end do
+    print 24,'lambda1c      = ', x0trim_val
+    print *,' '
+    print *,'lambda1s      = '
+    do i=1,const%nRot
+        index_calc = const%nRot*3+3+(i-1)*3
+        x0trim_val = x0trim(index_calc)
+    end do
+    print 24, x0trim_val
+    print *,' '
+    
+    ! trim coefficients of thrust and power, thrust and power 
+    print *,'PERFORMANCE METRICS'
+    do i=1,const%nRot
+        index_calc = 1+(i-1)
+        y0trim_val = y0trim(index_calc)
+    end do
+    print 15,'CT            = ', y0trim_val
+    do i=1,const%nRot
+        index_calc = const%nRot*1+1+(i-1)
+        y0trim_val = y0trim(index_calc)
+    end do
+    print 15,'CP            = ', y0trim_val
+    do i=1,const%nRot
+        index_calc = const%nRot*2+1+(i-1)
+        y0trim_val = y0trim(index_calc)
+    end do
+    print 52,'T [lb]        = ', y0trim_val
+    do i=1,const%nRot
+        index_calc = const%nRot*4+1+(i-1)
+        y0trim_val = y0trim(index_calc)
+    end do
+    print 52,'Q [lb-ft]     = ', y0trim_val
+    do i=1,const%nRot
+        index_calc = const%nRot*3+1+(i-1)
+        y0trim_val = y0trim(index_calc)
+    end do
+    print 52,'P [hp]        = ', y0trim_val
+    print *,' '
+    
+    ! trim controls 
+    print *,'ROTOR CONTROLS'
+    
+    do i=1,const%nRot
+        index_calc = 1+(i-1)*const%NCTRLS/const%nRot
+        u0trim_val = u0trim(index_calc)
+    end do
+    print 21,'theta0 [deg]  = ', u0trim_val
+    
+    do i=1,const%nRot
+        index_calc = 4+(i-1)*const%NCTRLS/const%nRot
+        u0trim_val = u0trim(index_calc)
+    end do
+    print 31,'Omega [rad/s] = ', u0trim_val
+    print *,' '
+        
+
+24  format(1x, f6.4)
+
+15  format(1x, f6.5)
+
+52  format(1x, f7.2)
+
+21  format(1x, f3.1)
+
+31  format(1x, f4.1)
 
 end program main
 
